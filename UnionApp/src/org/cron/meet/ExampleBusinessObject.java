cf27a843e9de5bff773ef9c8db3d63723748d0c4
Saurabh Dixit <saurabh.iips@gmail.com>
2016-10-02 10:46:28 +0530

    Added quartz dependency

---
+++ UnionApp/src/org/cron/meet/ExampleBusinessObject.java
@@ -0,0 +1,13 @@
+package org.cron.meet;
+
+public class ExampleBusinessObject {
+
+	    // properties and collaborators
+
+	    public void doIt() {
+	        System.out.println("I am ExampleBusinessObject cron!!");
+	    }
+}
+
+
+
