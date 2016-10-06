345b97c9c756acc44f003af0eb51999e84d90698
Saurabh Dixit <saurabh.iips@gmail.com>
2016-10-06 07:50:35 +0530

    Added BO for NL

---
+++ UnionApp/src/org/repository/entity/NewsLetterBO.java
@@ -0,0 +1,90 @@
+package org.repository.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class NewsLetterBO {
+	@Column(name = "subject")
+	private String subject;
+	
+	@Column(name = "nldate")
+	private Date nldate;
+	
+	@Column(name = "nltime")
+	private Date nltime;
+	
+	@Column(name = "detail")
+	private String detail;
+	
+	@Column(name = "creator")
+	private String creator;
+	
+	@Column(name = "status")
+	private String status;
+		
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "nlid")
+	private Integer nlid;
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Integer getNlid() {
+		return nlid;
+	}
+
+	public void setNlid(Integer nlid) {
+		this.nlid = nlid;
+	}
+
+	public Date getNldate() {
+		return nldate;
+	}
+
+	public void setNldate(Date nldate) {
+		this.nldate = nldate;
+	}
+
+	public Date getNltime() {
+		return nltime;
+	}
+
+	public void setNltime(Date nltime) {
+		this.nltime = nltime;
+	}	
+
+}
