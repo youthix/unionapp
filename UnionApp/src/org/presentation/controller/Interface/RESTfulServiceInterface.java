34835660277a874629da10b6e0934c9ebd872f98
saurabh agarwal <inbox.saurabh@gmail.com>
2016-10-27 15:47:49 +0530

    update

--- UnionApp/src/org/presentation/controller/Interface/RESTfulServiceInterface.java
+++ UnionApp/src/org/presentation/controller/Interface/RESTfulServiceInterface.java
@@ -15,35 +15,35 @@ import com.sun.jersey.multipart.FormDataParam;
 public interface RESTfulServiceInterface {
 
 	ResponseObj login(RequestObj reqparam);
-	
+
 	ResponseObj logout(RequestObj reqparam);
-	
+
 	ResponseObj register(RequestObj reqparam);
-	
+
 	ResponseObj fetch(RequestObj reqparam);
-	
+
 	ResponseObj update(RequestObj reqparam);
-	
+
 	ResponseObj updateuserprofile(RequestObj reqparam);
-	
+
 	ResponseObj updatepwd(RequestObj reqparam);
-	
+
 	ResponseObj resetpwd(RequestObj reqparam);
-	
+
 	ResponseObj createmeeting(RequestObj reqparam);
-	
+
 	ResponseObj updatemeeting(RequestObj reqparam);
-	
+
 	ResponseObj fetchmeeting(RequestObj reqparam);
-	
+
 	ResponseObj acceptdenymeeting(RequestObj reqparam);
-	
+
 	ResponseObj createactivity(RequestObj reqparam);
-	
+
 	ResponseObj updateactivity(RequestObj reqparam);
-	
+
 	ResponseObj fetchactivity(RequestObj reqparam);
-	
+
 	ResponseObj acceptdenyactivity(RequestObj reqparam);
 
 	ResponseObj fetchNewsLetter(RequestObj reqparam);
@@ -53,7 +53,7 @@ public interface RESTfulServiceInterface {
 	ResponseObj createNewsLetter(RequestObj reqparam);
 
 	String fetchNewsLetterById(String id);
-	
+
 	ResponseObj fetchAmr(RequestObj reqparam);
 
 	ResponseObj updateAmr(RequestObj reqparam);
@@ -61,7 +61,7 @@ public interface RESTfulServiceInterface {
 	ResponseObj createAmr(RequestObj reqparam);
 
 	String fetchAmrById(String id);
-	
+
 	ResponseObj fetchPayrate(RequestObj reqparam);
 
 	ResponseObj updatePayrate(RequestObj reqparam);
@@ -70,7 +70,6 @@ public interface RESTfulServiceInterface {
 
 	String fetchPayrateById(String id);
 
-	
 	ResponseObj fetchAgreement(RequestObj reqparam);
 
 	ResponseObj updateAgreement(RequestObj reqparam);
@@ -78,7 +77,7 @@ public interface RESTfulServiceInterface {
 	ResponseObj createAgreement(RequestObj reqparam);
 
 	String fetchAgreementById(String id);
-	
+
 	ResponseObj fetchSummary(RequestObj reqparam);
 
 	ResponseObj updateSummary(RequestObj reqparam);
@@ -86,22 +85,23 @@ public interface RESTfulServiceInterface {
 	ResponseObj createSummary(RequestObj reqparam);
 
 	String fetchSummaryById(String id);
-	
+
 	ResponseObj createsuggestionidea(RequestObj reqparam);
-	
-	ResponseObj updatesuggestionidea(RequestObj reqparam);	
-	
+
+	ResponseObj updatesuggestionidea(RequestObj reqparam);
+
 	ResponseObj fetchsuggestionidea(RequestObj reqparam);
-	
+
 	String fetchsuggestionideaById(String id);
-	
-	ResponseObj addcategory(RequestObj reqparam) ;
-	
+
+	ResponseObj addcategory(RequestObj reqparam);
+
 	ResponseObj fetchcategory(RequestObj reqparam);
-	
+
 	ResponseObj uploadFile(@FormDataParam("file") InputStream uploadedInputStream,
 			@FormDataParam("file") FormDataContentDisposition fileDetail,
 			@HeaderParam(value = "featureType") String featureType, @HeaderParam(value = "featureID") String featureId,
-			@HeaderParam(value = "attachmentType") String attachmentType);
+			@HeaderParam(value = "attachmentType") String attachmentType,
+			@HeaderParam(value = "attachmentTitle") String attachmentTitle);
 
 }
