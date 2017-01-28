b01bf6baebf950a2423b697186bf27f8fe319d78
Saurabh Dixit <saurabh.iips@gmail.com>
2017-01-28 19:34:18 +0530

    Updated for fcm functionality

--- UnionApp/src/org/service/utilityService/HttpClientUtil.java
+++ UnionApp/src/org/service/utilityService/HttpClientUtil.java
@@ -10,59 +10,37 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.common.UnionAppConstants;
 
 public class HttpClientUtil {
 
 	
-	public static void mainMethod(String[] args) throws Exception {
-
-		HttpClientUtil http = new HttpClientUtil();
-/*
-		System.out.println("Testing 1 - Send Http GET request");
-		http.sendGet();*/
-
-		System.out.println("\nTesting 2 - Send Http POST request");
-		http.sendPost();
-
-	}
-
 	// HTTP POST request
-	private void sendPost() throws Exception {
+	public static void sendNotification() throws Exception {
 
-		String url = "https://fcm.googleapis.com/fcm/send";
+		String url = UnionAppConstants.fcmServerUrl;
 
 		HttpClient client = new DefaultHttpClient();
 		HttpPost post = new HttpPost(url);
 		
 
-		// add header
-		//post.setHeader("User-Agent", USER_AGENT);
-		//post.setHeader("Authorization", "key=AIzaSyAnpZ6KII9z3W_JBJCLSM-WOcE7Nm2bMkA");
-		post.setHeader("Authorization", "key=AAAAozxQtE0:APA91bFh2JsmdW2hOqP3_psipNvFTpkLLCD1hzdZX5S-YFxpUIU8jlHxVJLZ6j-UcVeOH8r-GtFBdsgIkD7Ot6-cJl5UStjiH0lgAFaE3L3ZoVaCgdmN__xiZ3fwb-1mhhZpVcEWNJjh");
-		post.setHeader("Content-Type", "application/json");
-
-		/*List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
-		urlParameters.add(new BasicNameValuePair("sn", "C02G8416DRJM"));
-		urlParameters.add(new BasicNameValuePair("cn", ""));
-		urlParameters.add(new BasicNameValuePair("locale", ""));
-		urlParameters.add(new BasicNameValuePair("caller", ""));
-		urlParameters.add(new BasicNameValuePair("num", "12345"));
-		 	                    
-		post.setEntity(new UrlEncodedFormEntity(urlParameters));*/
-		
+		// add header		
+		post.setHeader("Authorization", "key="+UnionAppConstants.fcmServerKey);
+		post.setHeader("Content-Type", "application/json");		
 		
 		StringEntity requestEntity = new StringEntity(
 			    //"{\"data\":{\"score\":\"5x1\"},\"to\":\"eqNEPfDY-pU:APA91bHY7L2M_rBoCRGriP_Fhr6eFtf8ia4JCjYw3wPmVo4WBWhvjbNgBp2Jah8dLLueXLAp6FZtveuftQh_3yCdiSi8D434aqEBnPHGcZS5p0rueM2JppsC782yOGhgJVijLy8QNQzQ\"}",
 				//"{\"notification\":{\"apps\":{\"alert\":{\"body\" : \"Helllo\",\"title\" : \"Title\"}},\"badge\":\"1\",\"sound\":\"default\"},\"to\":\"eCP99TBwMQU:APA91bG1vwQfn7XhKodRe6HzrDRDq2daj006XPv1b6LyLGZYOsq3gnXQVM9QqG67CmTOgCbmCRXhVozlw8TDZVrScooIuNoA8srbwVXW5ZKluEhWhm9YPvrrAm2qKM7A4geGWbZgpGUf\"}",
-				"{\"notification\":{\"title\":\"Hello\",\"text\":\"Hello I am Saurabh\"},\"priority\":\"high\",\"to\":\"eCP99TBwMQU:APA91bG1vwQfn7XhKodRe6HzrDRDq2daj006XPv1b6LyLGZYOsq3gnXQVM9QqG67CmTOgCbmCRXhVozlw8TDZVrScooIuNoA8srbwVXW5ZKluEhWhm9YPvrrAm2qKM7A4geGWbZgpGUf\"}",
+				//"{\"notification\":{\"title\":\"Hello\",\"text\":\"Hej du har en besked fra WFS Klubben\"},\"priority\":\"high\",\"to\":\"eCP99TBwMQU:APA91bG1vwQfn7XhKodRe6HzrDRDq2daj006XPv1b6LyLGZYOsq3gnXQVM9QqG67CmTOgCbmCRXhVozlw8TDZVrScooIuNoA8srbwVXW5ZKluEhWhm9YPvrrAm2qKM7A4geGWbZgpGUf\"}",
+				"{\"notification\":{\"title\":\""+UnionAppConstants.notificationTitle+"\",\"text\":\""+UnionAppConstants.notififcaitonMessage+"\"},\"priority\":\"high\",\"to\":\"/topics/union\"}",
+				//"{\"notification\":{\"title\":\"Hello\",\"text\":\"Hej du har en besked fra WFS Klubben\"},\"priority\":\"high\",\"to\":\"eqNEPfDY-pU:APA91bHY7L2M_rBoCRGriP_Fhr6eFtf8ia4JCjYw3wPmVo4WBWhvjbNgBp2Jah8dLLueXLAp6FZtveuftQh_3yCdiSi8D434aqEBnPHGcZS5p0rueM2JppsC782yOGhgJVijLy8QNQzQ\"}",
 				//"{\"data\":[\"aps\":{\"alert\":{\"body\" : \"Helllo\",\"title\" : \"Title\"},\"badge\":\"1\",\"sound\":\"default\"},\"to\":\"eCP99TBwMQU:APA91bG1vwQfn7XhKodRe6HzrDRDq2daj006XPv1b6LyLGZYOsq3gnXQVM9QqG67CmTOgCbmCRXhVozlw8TDZVrScooIuNoA8srbwVXW5ZKluEhWhm9YPvrrAm2qKM7A4geGWbZgpGUf\"]}",
-			    ContentType.APPLICATION_JSON);
+		ContentType.APPLICATION_JSON);
 		post.setEntity(requestEntity);
 
 		HttpResponse response = client.execute(post);
-		System.out.println("\nSending 'POST' request to URL : " + url);
 		System.out.println("Post parameters : " + post.getEntity());
-		System.out.println("Response Code : " +
+		System.out.println("Notification Response Code : " +
                                     response.getStatusLine().getStatusCode());
 
 		BufferedReader rd = new BufferedReader(
@@ -75,7 +53,6 @@ public class HttpClientUtil {
 		}
 
 		System.out.println(result.toString());
-
 	}
 
 }
